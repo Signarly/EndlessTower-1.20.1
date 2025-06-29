c5510424886b93b7412bddeea1e9c9de1b320cc7
Sig <wompwomp91@gmail.com>
2025-06-28 20:52:48 -0700

    rest

---
+++ src/main/java/sig/mcmod/EndlessTower.java
@@ -0,0 +1,19 @@
+package sig.mcmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sig.mcmod.item.ModItems;
+
+public class EndlessTower implements ModInitializer {
+	public static final String MOD_ID = "endless-tower";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		ModItems.registerModItems();
+
+	}
+}
---
+++ src/main/java/sig/mcmod/EndlessTowerClient.java
@@ -0,0 +1,9 @@
+package sig.mcmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+public class EndlessTowerClient implements ClientModInitializer {
+@Override
+    public void onInitializeClient() {}
+}
